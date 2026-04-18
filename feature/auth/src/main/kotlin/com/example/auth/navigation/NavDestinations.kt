6dad0f75866be0af30f31fc58bb4799266e361b5
Яна Паймухина <yanapaim.vita@gmail.com>
2026-04-18 16:43:45 +0300

    combining navigation

--- feature/auth/src/main/kotlin/com/example/auth/navigation/NavDestinations.kt
+++
@@ -1,8 +0,0 @@
-package com.example.auth.navigation
-
-sealed class Screen(val route: String) {
-    object Startup : Screen("startup")
-    object Login : Screen("login")
-    object Register : Screen("register")
-    object Home : Screen("home")
-}
